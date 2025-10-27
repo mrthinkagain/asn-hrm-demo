2c3354bf9484bad433aea06fd1c65d54895af799
Your Name <your-email@example.com>
2025-10-28 01:33:59 +0530

    Initial commit: ASN Technologies HRMS - White-labeled and ready for deployment

---
+++ run_horilla.py
@@ -0,0 +1,13 @@
+﻿import os
+import sys
+import webbrowser
+import threading
+from django.core.management import execute_from_command_line
+
+def open_browser():
+    webbrowser.open('http://127.0.0.1:8000')
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'horilla.settings')
+    threading.Timer(3, open_browser).start()
+    execute_from_command_line(['manage.py', 'runserver', '--noreload'])
